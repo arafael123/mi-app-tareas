a1873e82abcc3564d4019f1df77dd80d488733c1
arafael123 <arafaelorozco@unicesar.edu.co>
2026-04-25 18:10:45 -0500

    Fix: código limpio con flake8

--- database.py
+++ database.py
@@ -1,13 +1,17 @@
 import sqlite3
 
+
 DB_NAME = "tasks.db"
+
 
 def get_connection():
     return sqlite3.connect(DB_NAME)
 
+
 def init_db():
     conn = get_connection()
     cursor = conn.cursor()
+
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS tasks (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,5 +19,6 @@ def init_db():
             completed INTEGER DEFAULT 0
         )
     """)
+
     conn.commit()
     conn.close()
